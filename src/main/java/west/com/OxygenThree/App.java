7234a17f4d5bbfedfe6970f1f98ce549f45c9cb7
frankwangtj@yahoo.ca <frankwangtj@yahoo.ca>
2018-02-08 08:58:52 -0500

    --Initial a project with setting remote and ignorefile

---
+++ src/main/java/west/com/OxygenThree/App.java
@@ -0,0 +1,13 @@
+package west.com.OxygenThree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
