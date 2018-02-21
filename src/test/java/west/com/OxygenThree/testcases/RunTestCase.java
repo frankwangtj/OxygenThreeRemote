621f0ccfca6f7a68aa4bbeeb327daf2d8a9c4dc2
frankwangtj@yahoo.ca <frankwangtj@yahoo.ca>
2018-02-21 18:24:17 -0500

    --customize login testcase into runtestcase class

--- src/test/java/west/com/OxygenThree/testcases/RunTestCase.java
+++ src/test/java/west/com/OxygenThree/testcases/RunTestCase.java
@@ -5,9 +5,13 @@ import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+
+import west.com.OxygenThree.pageObjects.OnlineStore_HomePage;
+import west.com.OxygenThree.pageObjects.OnlineStore_LoginPage;
 import west.com.OxygenThree.utility.Log;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.log4j.*;
+import java.util.concurrent.TimeUnit;
 
 public class RunTestCase {
 	public WebDriver driver;
@@ -19,7 +23,17 @@ public class RunTestCase {
 	// In other way before method is your prerequisites of your main Test Case	
 	@Test
 	public void RunSpecificTestCase() {
-		driver.get("http://www.google.com");
+		//driver.get("http://www.google.com");
+		Log.info("Launch onlinestore website");
+		driver.get("http://www.store.demoqa.com");
+		OnlineStore_HomePage.lnk_MyAccount(driver).click();
+		OnlineStore_LoginPage.txtbx_UserName(driver).sendKeys("testuser_12345678");
+		OnlineStore_LoginPage.txtbx_Password(driver).sendKeys("Test@123");
+		OnlineStore_LoginPage.btn_LogIn(driver).click();
+		System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
+		OnlineStore_HomePage.lnk_LogOut(driver).click();
+		System.out.println(" Logout Successfully.");
+		
 	}
 	@BeforeMethod
 	public void beforeMethod() {
@@ -32,21 +46,22 @@ public class RunTestCase {
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		System.out.println("Start to test--RunTestCase");
-		System.out.println("Add from slave machine");
+		//System.out.println("Add from slave machine");
 		//Update at slave after second clone
 		//Update by master machine after cloned
 		//update from slave one more time
 		//Submit update
 		//Modify on slave after USB key setup
 		//Modify and sync with slave
-		Log.info("Test started--RunTestCase");
-		Log.debug("output on console");
-		Log.debug("test");
-		Log.error("sth wrong");
-		Log.startTestCase("RunTestCase");
-		Log.endTestCase("RunTestCase");
-		Log.warn("warning");
-		Log.step("step1");
+		//Log.info("Test started--RunTestCase");
+		//Log.debug("output on console");
+		//Log.debug("test");
+		//Log.error("sth wrong");
+		//Log.startTestCase("RunTestCase");
+		//Log.endTestCase("RunTestCase");
+		//Log.warn("warning");
+		//Log.step("step1");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	
 	}
 
