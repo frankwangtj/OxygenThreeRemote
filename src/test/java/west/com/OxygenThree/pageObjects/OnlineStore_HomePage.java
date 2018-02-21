5c49ab017dd7f0d9cd4353ae532a51fe6c521585
frankwangtj@yahoo.ca <frankwangtj@yahoo.ca>
2018-02-21 08:50:41 -0500

    --Correct Snippet.java and implement homepage in PageObjects

---
+++ src/test/java/west/com/OxygenThree/pageObjects/OnlineStore_HomePage.java
@@ -0,0 +1,18 @@
+package west.com.OxygenThree.pageObjects;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class OnlineStore_HomePage {
+	private static WebElement element = null;
+	public static WebElement lnk_MyAccount(WebDriver driver){
+		element = driver.findElement(By.id("account"));
+	    return element;
+	}
+
+	 public static WebElement lnk_LogOut(WebDriver driver){
+	    element = driver.findElement(By.id("account_logout"));
+	    return element;
+	 }
+
+}
