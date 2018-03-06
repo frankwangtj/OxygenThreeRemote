1cd4b29f276d2aaa946bfb95bb3458c975af831b
frankwangtj@yahoo.ca <frankwangtj@yahoo.ca>
2018-03-06 08:20:38 -0500

    --update homepage

--- src/test/java/west/com/OxygenThree/pageObjects/OnlineStore_LoginPage.java
+++ src/test/java/west/com/OxygenThree/pageObjects/OnlineStore_LoginPage.java
@@ -7,13 +7,14 @@ import org.openqa.selenium.WebElement;
 public class OnlineStore_LoginPage {
 	private static WebElement element = null;
 	public static WebElement txtbx_UserName(WebDriver driver){
-		element = driver.findElement(By.id("log"));
+		element = driver.findElement(By.name("log"));
 		return element;
 	}
 
 	public static WebElement txtbx_Password(WebDriver driver){
-		element = driver.findElement(By.id("pwd"));
+		element = driver.findElement(By.name("pwd"));
 		return element;
+		//
 	}
 
 	public static WebElement btn_LogIn(WebDriver driver){
